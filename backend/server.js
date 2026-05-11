11c63efc917e353efa63ddf444cd4794d79caad9
RipsiShamilyan <shamilyan.ripsi@gmail.com>
2026-05-11 23:24:37 +0400

    Fix mysql auth issue with mysql2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const multer = require('multer');
